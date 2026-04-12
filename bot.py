450f0452500e01e8cc4c01e6d3fa4d3049456792
fuserwyn <90343030+fuserwyn@users.noreply.github.com>
2026-04-13 00:48:39 +0300

    [AI job #24] bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,27 @@
-import logging
-import os
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.utils import executor
+import logging
 
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-)
-logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 
-API_TOKEN = os.getenv('TELEGRAM_API_TOKEN')
+API_TOKEN = 'YOUR_BOT_TOKEN'
 
 bot = Bot(token=API_TOKEN)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
-logger.info('Бот запущен')
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    await message.reply("Hi! I'm a bot.")
+
+@dp.message_handler(commands=['help'])
+async def send_help(message: types.Message):
+    await message.reply("Available commands: /start, /help, /newcmd")
+
+@dp.message_handler(commands=['newcmd'])
+async def new_command(message: types.Message):
+    await message.reply("This is a new command!")
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
