4cf1827f2b7123e3929910e30e904b54261f48d0
fuserwyn <90343030+fuserwyn@users.noreply.github.com>
2026-04-13 02:31:16 +0300

    [AI job #67] config.py

--- config.py
+++ config.py
@@ -3,5 +3,4 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "").split(","))) if os.getenv("ADMIN_IDS") else []
